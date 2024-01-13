ae43f3706575567010d603542637a0bc241c4b26
Mateusz Wodzynski <wodzynski.mateusz@gmail.com>
2024-01-13 16:00:15 +0100

    adding photo and main

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import cv2
 
-img = cv2.imread('photo.jpg')
-print(type(img))
-print(img.shape)
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-cv2.imshow('image', img)
+# load image
+image = cv2.imread('photo.jpg')
+image = cv2.resize(image, (650, 400))
+
+# detect people in the image
+(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+# draw the bounding boxes
+for (x, y, w, h) in rects:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+print(f'Found {len(rects)} humans')
+
+# show the output images
+cv2.imshow("People detector", image)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
