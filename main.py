542a5a9c9fd077bcf6ce724735a0967c9aab8cc6
Mateusz Wodzynski <wodzynski.mateusz@gmail.com>
2024-01-14 10:25:04 +0100

    adding get method

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import cv2
+from flask import Flask
+from flask_restful import Resource, Api
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# load image
-image = cv2.imread('photo.jpg')
-image = cv2.resize(image, (650, 400))
+app = Flask(__name__)
+api = Api(app)
 
-# detect people in the image
-(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+class PeopleCounter(Resource):
+    def get(self):
+        # load image
+        image = cv2.imread('photo.jpg')
+        image = cv2.resize(image, (670, 420))
 
-# draw the bounding boxes
-for (x, y, w, h) in rects:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
-print(f'Found {len(rects)} humans')
+        return {'peopleCount': len(rects)}
 
-# show the output images
-cv2.imshow("People detector", image)
-cv2.waitKey(0)
+
+api.add_resource(PeopleCounter, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
