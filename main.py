eb7122457c5d02bf06860917cd04f6ae847bcaa8
Mateusz Wodzynski <wodzynski.mateusz@gmail.com>
2024-01-14 15:37:24 +0100

    adding dynamic counter

--- main.py
+++ main.py
@@ -1,28 +1,56 @@
 import cv2
-from flask import Flask
+import requests
+from flask import Flask, request
 from flask_restful import Resource, Api
-
-# initialize the HOG descriptor/person detector
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class PeopleCounter(Resource):
+def count_people(path):
+    # initialize the HOG descriptor/person detector
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    # load and resize image
+    image = cv2.imread(path)
+    image = cv2.resize(image, (750, 450))
+
+    # detect people in the image
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+    return len(rects)
+
+
+class PeopleCounterStatic(Resource):
     def get(self):
-        # load image
-        image = cv2.imread('photo.jpg')
-        image = cv2.resize(image, (670, 420))
+        path = "photo.jpg"
 
-        # detect people in the image
-        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        # count people
+        counter = count_people(path)
 
-        return {'peopleCount': len(rects)}
+        return {'peopleCount': counter}
 
 
-api.add_resource(PeopleCounter, '/')
+class PeopleCounterDynamicUrl(Resource):
+    def get(self):
+        photo_path = "downloaded_photo.jpg"
+
+        # get url from the request
+        url = request.args.get('url')
+
+        # get picture from url and save
+        picture = requests.get(url).content
+        with open(photo_path, "wb") as f:
+            f.write(picture)
+
+        # count people
+        counter = count_people(photo_path)
+        return {'peopleCount': counter}
+
+
+api.add_resource(PeopleCounterStatic, '/')
+api.add_resource(PeopleCounterDynamicUrl, '/dynamic')
 
 if __name__ == '__main__':
     app.run(debug=True)
